58c1ff9f488b21746503fe936f588411a4306424
rena <renanurhasanah95@gmail.com>
2026-04-12 10:40:19 +0700

    update db config

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$dbname = 'ceritakita';
+$host   = 'mysql.railway.internal';
+$user   = 'root';
+$pass   = 'ObrJZrhunNccQSPFzWctObbAkXHvwfjk';
+$dbname = 'railway';
+$port   = 3306;
 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
